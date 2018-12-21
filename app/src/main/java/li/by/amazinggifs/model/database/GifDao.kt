bed7001fe954b39456f7522b1530dbaa6aec6301
Ilya Sysoi <14ilya12322@gmail.com>
2018-12-21 15:49:54 +0300

    add model and dao

---
+++ app/src/main/java/li/by/amazinggifs/model/database/GifDao.kt
@@ -0,0 +1,22 @@
+package li.by.amazinggifs.model.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+
+@Dao
+public interface GifDao {
+
+    @Insert
+    fun insert(gif: GifDatabaseModel)
+
+    @Delete
+    fun delete(gif: GifDatabaseModel)
+
+    @Query("SELECT * FROM gif")
+    fun getAllGifs(): LiveData<GifDatabaseModel>
+
+}
